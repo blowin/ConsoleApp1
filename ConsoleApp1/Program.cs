9fb6e06fa7ca39fd7ec010899f2d0ac71f3a0213
Dmitry Sushchevsky <d.suschevsky.personal@gmail.com>
2022-12-11 10:33:16 +0300

    Added documentation to Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,14 @@
 namespace ConsoleApp1;
 
+/// <summary>
+/// Main program.
+/// </summary>
 public static class Program
 {
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    /// <param name="args">Args of application.</param>
     public static void Main(string[] args)
     {
     }
